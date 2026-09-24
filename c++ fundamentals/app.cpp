5716abdbc50b4eb50ff495b6dc88ecc27b8674e8
Basmala Elshenawy <basmalaelshenawy03@gmail.com>
2026-09-24 03:56:26 +0300

    C++ ProblemSolving

---
+++ c++ fundamentals/app.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std ;
+ int main()
+ 
+// {
+//     std::cout << "I am Here";
+//     return 0;
+// }
+
+// {
+//     int price = 500;
+//     cout << "price is : " << price ;
+//     cout << " \n price before add coast 12 : " << price + 12 ;
+//     cout << "\n price after descond 10: " << price - 10 ;
+
+//     price = 300;
+
+//     cout << " \n price after update  :  "  << price ;
+
+// }
