70f08a599986369545a9e2bf3dbace7e7d821447
Basmala Elshenawy <basmalaelshenawy03@gmail.com>
2026-09-25 08:23:28 +0300

    Initial commit

--- c++ fundamentals/app.cpp
+++ c++ fundamentals/app.cpp
@@ -2,7 +2,7 @@
 using namespace std ;
  int main()
  
-// {
+ {
 //     std::cout << "I am Here";
 //     return 0;
 // }
@@ -17,4 +17,41 @@ using namespace std ;
 
 //     cout << " \n price after update  :  "  << price ;
 
-// }
+//! we have a company  and we want to pt the values of the working houers = 8 hr and employee has salary = 15000  and we can change it . 
+
+// const int houresDay = 8;
+// int Salary =15000;
+
+// Salary = 30000;
+
+// cout << "working houres of Day : "  << houresDay;
+// cout << "\n  Salary of employee : " << Salary ;
+
+//? Calculate your Age Application
+
+// int birthday = 2005 ;
+// int year = 2026 ;
+
+// cout << " your  Age  for Now : " << year - birthday ;
+
+//* Another solve 
+
+// cout << "=======================================\n";
+// cout << "****Calculate your Age Application****\n";
+// cout << "=======================================\n";
+
+// int birthday ;
+//  int currentAge = 2026 ;
+//  int age_in_dayes = birthday * 365 ;
+//  int age_in_houres = age_in_dayes * 24 ;
+//  cout << " Enter yor birthday : " ;
+//  cin >> birthday;
+//  cout << " your Age is :  " << currentAge - birthday ;
+//  cout << " \nage in dayes : " << age_in_dayes ;
+//  cout << " \nage _in_houres : " << age_in_houres ;
+ 
+ 
+
+
+return 0;
+ }
